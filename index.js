cef2dd7a97bb03392fc7254e9da7f26434ae8c8b
ttran9 <toddtran9@gmail.com>
2021-05-26 13:50:18 -0700

    creating connection to our database.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // const routes = require("./routes/api");
+const mongoose = require("mongoose");
 
 // set up express app.
 const app = express();
+
+// connect to mongodb.
+mongoose.connect("mongodb://localhost/ninjago", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.Promise = global.Promise; // override mongoose's promise.
 
 // set up bodyParser middleware.
 app.use(bodyParser.json());
